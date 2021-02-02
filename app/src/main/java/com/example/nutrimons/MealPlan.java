38bfaac8d1f90321e3f91711d0f743ca29697bef
alissakc <44853025+alissakc@users.noreply.github.com>
2021-02-02 12:07:07 -0800

    Meal Plan fragment has been updated to look like the React Native screen. The dropdown menu has been implemented. Still need to work on submitting the selected items.

--- app/src/main/java/com/example/nutrimons/MealPlan.java
+++ app/src/main/java/com/example/nutrimons/MealPlan.java
@@ -1,19 +1,24 @@
 package com.example.nutrimons;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MealPlan#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MealPlan extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+import androidx.fragment.app.Fragment;
+
+
+public class MealPlan extends Fragment implements OnItemSelectedListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +28,9 @@ public class MealPlan extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // vars
+    private Spinner spinner1, spinner2, spinner3, spinner4;
 
     public MealPlan() {
         // Required empty public constructor
@@ -49,16 +57,65 @@ public class MealPlan extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meal_plan, container, false);
+        View view = inflater.inflate(R.layout.fragment_meal_plan, container, false);
+
+        // Spinner element
+        spinner1 = (Spinner) view.findViewById(R.id.breakfastMultipleItemSelectionSpinner);
+        spinner2 = (Spinner) view.findViewById(R.id.lunchMultipleItemSelectionSpinner);
+        spinner3 = (Spinner) view.findViewById(R.id.dinnerMultipleItemSelectionSpinner);
+        spinner4 = (Spinner) view.findViewById(R.id.snackMultipleItemSelectionSpinner);
+
+        // Spinner click listener
+        spinner1.setOnItemSelectedListener(this);
+        spinner2.setOnItemSelectedListener(this);
+        spinner3.setOnItemSelectedListener(this);
+        spinner4.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> food = new ArrayList<String>();
+        food.add("Select an item");
+        food.add("Apple Pie");
+        food.add("Banana");
+        food.add("Chicken Noodle Soup");
+        food.add("Eggs");
+        food.add("Greek Yogurt");
+        food.add("Tomato Sauce Pasta");
+        food.add("White Rice");
+
+        // Creating adapter for spinners
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, food);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinners
+        spinner1.setAdapter(dataAdapter);
+        spinner2.setAdapter(dataAdapter);
+        spinner3.setAdapter(dataAdapter);
+        spinner4.setAdapter(dataAdapter);
+
+
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 }
