084cef44f4b07605f704468dac10565bc290e306
alissakc <44853025+alissakc@users.noreply.github.com>
2021-02-02 19:29:04 -0800

    Update the Meal page to navigate between fragments

--- app/src/main/java/com/example/nutrimons/Meal.java
+++ app/src/main/java/com/example/nutrimons/Meal.java
@@ -2,18 +2,26 @@ package com.example.nutrimons;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Meal#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Meal extends Fragment {
+public class Meal extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +31,9 @@ public class Meal extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // vars
+    Button gotoScanBarcode, gotoAddMeal, gotoMealPlan;
 
     public Meal() {
         // Required empty public constructor
@@ -59,6 +70,30 @@ public class Meal extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meal, container, false);
+        View view = inflater.inflate(R.layout.fragment_meal, container, false);
+
+        // Button initialization
+        gotoScanBarcode = view.findViewById(R.id.scanBarcodeButton);
+        gotoAddMeal = view.findViewById(R.id.addMealButton);
+        gotoMealPlan = view.findViewById(R.id.mealPlanButton);
+
+        // assign listener for buttons
+        gotoScanBarcode.setOnClickListener(this);
+        gotoAddMeal.setOnClickListener(this);
+        gotoMealPlan.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.findViewById(R.id.scanBarcodeButton).equals(view)) {
+            Navigation.findNavController(view).navigate(R.id.action_nav_meal_to_nav_scanBarcode);
+        } else if (view.findViewById(R.id.addMealButton).equals(view)) {
+            Navigation.findNavController(view).navigate(R.id.action_nav_meal_to_nav_addMeal);
+        } else if (view.findViewById(R.id.mealPlanButton).equals(view)) {
+            Navigation.findNavController(view).navigate(R.id.action_nav_meal_to_nav_mealPlan);
+        }
+    }
+
 }
