d9dad25c978ce3c11d006713e8bfb8789af6c174
alissakc <44853025+alissakc@users.noreply.github.com>
2021-02-04 18:26:52 -0800

    Removed some unused imports

--- app/src/main/java/com/example/nutrimons/Meal.java
+++ app/src/main/java/com/example/nutrimons/Meal.java
@@ -2,18 +2,12 @@ package com.example.nutrimons;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 
 /**
