222c6a9771f411aff5310ca4fd0b9e1f71067ee9
MouseMan2 <jeemouseman2@gmail.com>
2021-02-11 22:37:48 -0800

    added in nutrients recommendation button to profile determined cause of crash on clicking the pencil icon - see comment in Profile.java added user table columns

--- app/src/main/java/com/example/nutrimons/Profile.java
+++ app/src/main/java/com/example/nutrimons/Profile.java
@@ -13,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -38,10 +39,12 @@ public class Profile extends Fragment implements View.OnClickListener, AdapterVi
 
     private Spinner spinner;
 
-    private Button editProfilePicture;
+    private Button editProfilePicture, showNutrientRecs;
 
-    private String profileFocus, name, username, password, birthday, financialSource, financialHistory, financialPlan, nutriCoins, age, sex, weight, height, ethnicity, healthHistory, healthGoals;
-    private EditText nameText, usernameText, passwordText, birthdayText, financialSourceText, financialHistoryText, financialPlanText, nutriCoinsText, ageText, sexText, weightText, heightText, ethnicityText, healthHistoryText, healthGoalsText;
+    private String profileFocus, name, email, password, birthday, financialSource, financialHistory, financialPlan, nutriCoins, age, sex, weight, height, ethnicity, healthHistory, healthGoals, activityLevel;
+    private EditText nameText, emailText, passwordText, birthdayText, financialSourceText, financialHistoryText, financialPlanText, nutriCoinsText, ageText, sexText, weightText, heightText, ethnicityText, healthHistoryText, healthGoalsText, activityLevelText;
+
+    private View view;
 
     public Profile() {
         // Required empty public constructor
@@ -77,7 +80,7 @@ public class Profile extends Fragment implements View.OnClickListener, AdapterVi
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         //set up button
         /*editProfilePicture = view.findViewById(R.id.imageView5);
@@ -117,8 +120,14 @@ public class Profile extends Fragment implements View.OnClickListener, AdapterVi
         // attaching data adapter to spinners
         spinner.setAdapter(dataAdapter);
 
+        //editProfilePicture = view.findViewById(R.id.imageView5); cannot cast the ImageView as a Button; use an ImageButton object instead
+        showNutrientRecs = view.findViewById(R.id.showNutrientRecs);
+
+        //editProfilePicture.setOnClickListener(this);
+        showNutrientRecs.setOnClickListener(this);
+
         nameText = view.findViewById(R.id.profileName);
-        usernameText = view.findViewById(R.id.username);
+        emailText = view.findViewById(R.id.email);
         passwordText = view.findViewById(R.id.password);
         birthdayText = view.findViewById(R.id.birthday);
         financialSourceText = view.findViewById(R.id.financialSource);
@@ -132,9 +141,10 @@ public class Profile extends Fragment implements View.OnClickListener, AdapterVi
         ethnicityText = view.findViewById(R.id.ethnicity);
         healthHistoryText = view.findViewById(R.id.healthHistory);
         healthGoalsText = view.findViewById(R.id.healthGoals);
+        activityLevelText = view.findViewById(R.id.activityLevel);
 
         nameText.addTextChangedListener(new TextChangedListener<EditText>(nameText, name));
-        usernameText.addTextChangedListener(new TextChangedListener<EditText>(usernameText, username));
+        emailText.addTextChangedListener(new TextChangedListener<EditText>(emailText, email));
         passwordText.addTextChangedListener(new TextChangedListener<EditText>(passwordText, password));
         birthdayText.addTextChangedListener(new TextChangedListener<EditText>(birthdayText, birthday));
         financialSourceText.addTextChangedListener(new TextChangedListener<EditText>(financialSourceText, financialSource));
@@ -148,14 +158,23 @@ public class Profile extends Fragment implements View.OnClickListener, AdapterVi
         ethnicityText.addTextChangedListener(new TextChangedListener<EditText>(ethnicityText, ethnicity));
         healthHistoryText.addTextChangedListener(new TextChangedListener<EditText>(healthHistoryText, healthHistory));
         healthGoalsText.addTextChangedListener(new TextChangedListener<EditText>(healthGoalsText, healthGoals));
+        activityLevelText.addTextChangedListener(new TextChangedListener<EditText>(activityLevelText, activityLevel));
 
         return view;
     }
 
     @Override
     public void onClick(View view) {
-        //let user upload photo and swap user profile pic with this
-        Toast.makeText(getContext(), "Pencil Clicked", Toast.LENGTH_LONG).show();
+        switch(view.getId()) {
+            case (R.id.imageView5): //let user upload photo and swap user profile pic with this
+                Toast.makeText(getContext(), "Pencil Clicked", Toast.LENGTH_LONG).show();
+                //Navigation.findNavController(view).navigate(R.id.action_nav_meal_to_nav_scanBarcode);
+                break;
+            case (R.id.showNutrientRecs):
+                calculateNutrients();
+                Toast.makeText(getContext(), "nutrient button clicked", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
     @Override
@@ -204,5 +223,80 @@ public class Profile extends Fragment implements View.OnClickListener, AdapterVi
             str = et.getText().toString();
             Toast.makeText(getContext(), "value is now: " + str, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void calculateNutrients()
+    {
+        View nt = view.findViewById(R.id.nutrientsTable);
+        TextView rdiCal = view.findViewById(R.id.rdiCalories);
+        TextView ulCal = view.findViewById(R.id.ulCalories);
+        TextView rdiPro = view.findViewById(R.id.rdiProtein);
+        TextView ulPro = view.findViewById(R.id.ulProtein);
+        TextView rdiCarbs = view.findViewById(R.id.rdiCarbs);
+        TextView ulCarbs = view.findViewById(R.id.ulCarbs);
+        TextView rdiFats = view.findViewById(R.id.rdiFats);
+        TextView ulFats = view.findViewById(R.id.ulFats);
+        int calories, proteins, proteinMax, carbohydrates, carbMax, fats, fatMax;
+
+        getFields();
+        calories = calculateCalories();
+        proteins = (int)(calories * .2125); //10-35% calorie recommendation
+        proteinMax = (int)(calories * .35);
+        carbohydrates = (int)(calories * .525); //45-65% calorie recommendation
+        carbMax = (int)(calories * .65);
+        fats = (int)(calories * .2625); //20-35% calorie recommendation
+        fatMax = (int)(calories * .35);
+
+        rdiCal.setText(String.valueOf(calories));
+        ulCal.setText(String.valueOf(calories));
+        rdiPro.setText(String.valueOf(proteins));
+        ulPro.setText(String.valueOf(proteinMax));
+        rdiCarbs.setText(String.valueOf(carbohydrates));
+        ulCarbs.setText(String.valueOf(carbMax));
+        rdiFats.setText(String.valueOf(fats));
+        ulFats.setText(String.valueOf(fatMax));
+
+        nt.setVisibility(view.VISIBLE);
+    }
+
+    private void getFields()
+    {
+        name = nameText.getText().toString();
+        email = emailText.getText().toString();
+        password = passwordText.getText().toString();
+        birthday = birthdayText.getText().toString();
+        financialSource = financialSourceText.getText().toString();
+        financialHistory = financialHistoryText.getText().toString();
+        financialPlan = financialPlanText.getText().toString();
+        nutriCoins = nutriCoinsText.getText().toString();
+        age = ageText.getText().toString();
+        sex = sexText.getText().toString();
+        weight = weightText.getText().toString();
+        height = heightText.getText().toString();
+        ethnicity = ethnicityText.getText().toString();
+        healthHistory = healthHistoryText.getText().toString();
+        healthGoals = healthGoalsText.getText().toString();
+        activityLevel = activityLevelText.getText().toString();
+    }
+
+    private int calculateCalories()
+    {
+        double al;
+        //modify with activity modifier https://www.k-state.edu/paccats/Contents/PA/PDF/Physical%20Activity%20and%20Controlling%20Weight.pdf
+        if(activityLevel.equals("Sedentary"))
+            al = 1.2;
+        else if(activityLevel.equals("Lightly active"))
+            al = 1.375;
+        else if(activityLevel.equals("Moderately active"))
+            al = 1.55;
+        else if(activityLevel.equals("Very active"))
+            al = 1.725;
+        else
+            al = 1.9;
+
+        if(sex.equals("Male")) //Mifflin-St Jeor Equation https://en.wikipedia.org/wiki/Basal_metabolic_rate
+            return (int)((10 * Integer.parseInt(weight) / 2.2 /*pounds*/ + 6.25 * 2.54 * Integer.parseInt(height) /*inches*/ - 5 * Integer.parseInt(age) + 5) * al);
+        else
+            return (int)((10 * Integer.parseInt(weight) / 2.2 /*pounds*/ + 6.25 * 2.54 * Integer.parseInt(height) /*inches*/ - 5 * Integer.parseInt(age) - 161) * al);
     }
 }
--- app/src/main/java/com/example/nutrimons/database/User.java
+++ app/src/main/java/com/example/nutrimons/database/User.java
@@ -3,8 +3,11 @@ package com.example.nutrimons.database;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 import androidx.room.Index;
+
+import com.example.nutrimons.R;
 
 @Entity//(indices = {@Index(value = {"user_name"}, unique = false)})
 public class User {
@@ -21,11 +24,72 @@ public class User {
     @ColumnInfo(name = "password")
     public String password;
 
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "birthday")
+    public String birthday;
+
+    @ColumnInfo(name = "financialSource")
+    public String financialSource;
+
+    @ColumnInfo(name = "financialHistory")
+    public String financialHistory;
+
+    @ColumnInfo(name = "financialPlan")
+    public String financialPlan;
+
+    @ColumnInfo(name = "nutriCoins")
+    public String nutriCoins;
+
+    @ColumnInfo(name = "age")
+    public String age;
+
+    @ColumnInfo(name = "sex")
+    public String sex;
+
+    @ColumnInfo(name = "weight")
+    public String weight;
+
+    @ColumnInfo(name = "height")
+    public String height;
+
+    @ColumnInfo(name = "ethnicity")
+    public String ethnicity;
+
+    @ColumnInfo(name = "healthHistory")
+    public String healthHistory;
+
+    @ColumnInfo(name = "healthGoals")
+    public String healthGoals;
+
+    @ColumnInfo(name = "profileFocus")
+    public String profileFocus;
+
     public User(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
+    @Ignore
+    public User(String email, String password, String name, String birthday, String financialSource, String financialHistory, String financialPlan, String nutriCoins, String age, String sex, String weight, String height, String ethnicity, String healthHistory, String healthGoals, String profileFocus) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.birthday = birthday;
+        this.financialSource = financialSource;
+        this.financialHistory = financialHistory;
+        this.financialPlan = financialPlan;
+        this.nutriCoins = nutriCoins;
+        this.age = age;
+        this.sex = sex;
+        this.weight = weight;
+        this.height = height;
+        this.ethnicity = ethnicity;
+        this.healthHistory = healthHistory;
+        this.healthGoals = healthGoals;
+        this.profileFocus = profileFocus;
+    }
 
     /*public User(String userName, String email, String password) {
         this.userName = userName;
